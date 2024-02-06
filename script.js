20209d22549e88f3d29c7ab757a641c401a25d52
dev <154620795+frontdanubius@users.noreply.github.com>
2024-02-06 13:40:43 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Change the tutorial phrase dynamically
+    const tutorialPhraseElement = document.getElementById('tutorialPhrase');
+    tutorialPhraseElement.textContent = 'Como fazer o deployment do site.';
+  
+    // You can add more dynamic behavior or functionality here.
+  });
+  
